3824b316f5368efaa92b45275bf25bc5cf6be81c
M. Lammers <60792074+martijnlammers@users.noreply.github.com>
2022-09-24 10:23:04 +0200

    Updated db/api-calls with History retrieval

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,40 @@
 import * as express from "express";
-import { PrismaClient } from '@prisma/client'
-const app = express()
+import { PrismaClient, Sim_history } from '@prisma/client'
+import { v4 as uuidv4 } from 'uuid';
+
+const app = express();
 app.use(express.json());
-const port = 3000
-const prisma = new PrismaClient()
-
-interface IEvent {
-  user_id: string
-}
+const port = 3000;
+const prisma = new PrismaClient();
 
 
-app.post('/simulation/event', async (req, res) => {
-  const results = await prisma.sim_event.create({
+app.post('/simulation/history', async (req, res) => {
+  const result = await prisma.sim_history.create({
     data: {
-      user_id: req.body['user_id'],
+      id: `${uuidv4()}`,
     },
-  })
+  });
+  res.send(result);
+})
 
-  res.send(results)
+app.get('/simulation/history', async (req, res) => {
+  let result: any;
+  if(req.body['id'] != null){
+    result = await prisma.sim_history.findUnique({
+      where: {
+        id: req.body['id'],
+      },
+    });
+  } else {
+    result = await prisma.sim_history.findMany()
+  }
+  
+  res.send(result);
+})
+
+app.delete('/simulation/history', async (req, res) => {
+  const result = await prisma.sim_history.deleteMany()
+  res.send(result);
 })
 
 app.delete('/simulation/event', async (req, res) => {
@@ -25,8 +42,8 @@ app.delete('/simulation/event', async (req, res) => {
     where: {
       user_id: req.body['user_id'],
     },
-  })
-  res.send(results)
+  });
+  res.send(results);
 })
 
 app.get('/simulation/event', async (req, res) => {
@@ -34,10 +51,10 @@ app.get('/simulation/event', async (req, res) => {
     where: {
       user_id: req.body['user_id'],
     },
-  })
-  res.send(results)
+  });
+  res.send(results);
 })
 
 const server = app.listen(3000, () =>
   console.log(`🚀 Server ready at: http://localhost:3000`),
-)
+);
