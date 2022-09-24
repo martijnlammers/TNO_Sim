b368cd798844abf4e1e7bbafd848e7cc4c650364
M. Lammers <60792074+martijnlammers@users.noreply.github.com>
2022-09-24 11:22:13 +0200

    Cleanup commit

--- dist/server.js
+++ dist/server.js
@@ -38,27 +38,86 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 exports.__esModule = true;
 var express = require("express");
 var client_1 = require("@prisma/client");
+var uuid_1 = require("uuid");
 var app = express();
 app.use(express.json());
 var port = 3000;
 var prisma = new client_1.PrismaClient();
-app.post('/event', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var user, results;
+app.post('/simulation/history', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var result;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, prisma.sim_history.create({
+                    data: {
+                        id: "".concat((0, uuid_1.v4)())
+                    }
+                })];
+            case 1:
+                result = _a.sent();
+                res.send(result);
+                return [2 /*return*/];
+        }
+    });
+}); });
+app.get('/simulation/history', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var result;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                if (!(req.body['id'] != null)) return [3 /*break*/, 2];
+                return [4 /*yield*/, prisma.sim_history.findUnique({
+                        where: {
+                            id: req.body['id']
+                        },
+                        include: {
+                            events: true
+                        }
+                    })];
+            case 1:
+                result = _a.sent();
+                return [3 /*break*/, 4];
+            case 2: return [4 /*yield*/, prisma.sim_history.findMany()];
+            case 3:
+                result = _a.sent();
+                _a.label = 4;
+            case 4:
+                res.send(result);
+                return [2 /*return*/];
+        }
+    });
+}); });
+app["delete"]('/simulation/history', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var result;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, prisma.sim_history.deleteMany()];
+            case 1:
+                result = _a.sent();
+                res.send(result);
+                return [2 /*return*/];
+        }
+    });
+}); });
+// app.get('/simulation/event', async (req, res) => {
+//   const results = await prisma.sim_event.findMany({
+//     where: {
+//       user_id: req.body['user_id'],
+//     },
+//   });
+//   res.send(results);
+// })
+app.post('/simulation/event', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var results;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0: return [4 /*yield*/, prisma.sim_event.create({
                     data: {
-                        user_id: req.body['user_id']
+                        user_id: req.body['user_id'],
+                        action: req.body['action'],
+                        history_id: req.body['history_id']
                     }
                 })];
             case 1:
-                user = _a.sent();
-                return [4 /*yield*/, prisma.sim_event.findMany({
-                        where: {
-                            user_id: 'martijn'
-                        }
-                    })];
-            case 2:
                 results = _a.sent();
                 res.send(results);
                 return [2 /*return*/];
