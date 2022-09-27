47f5d6b556cf7ec5b583d2bec2469429c271a5c9
M. Lammers <60792074+martijnlammers@users.noreply.github.com>
2022-09-27 15:08:44 +0200

    Added create user endpoint.

---
+++ src/models.ts
@@ -0,0 +1,16 @@
+enum EventType{
+    TAKE = "grabbed",
+    DROP = "dropped",
+    USE  = "uses"
+}
+
+enum GlassesType{
+
+}
+
+enum Roles{
+    TRAINEE = "TRAINEE",
+    SUPERVISOR = "SUPERVISOR"
+}
+
+export {EventType, GlassesType, Roles}
--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,44 @@
 import * as express from "express";
-import { PrismaClient, Sim_history } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 import { v4 as uuidv4 } from 'uuid';
+import * as m from './models'
 
 const app = express();
 app.use(express.json());
 const port = 3000;
 const prisma = new PrismaClient();
 
-app.post('/simulation/history', async (req, res) => {
-  const result = await prisma.sim_history.create({
-    data: {
-      id: `${uuidv4()}`,
-    }
-  });
-  res.send(result);
-})
+// app.post('/simulation/history', async (req, res) => {
+//   const result = await prisma.sim_history.create({
+//     data: {
+//       id: `${uuidv4()}`,
+//     }
+//   });
+//   res.send(result);
+// })
 
-app.get('/simulation/history', async (req, res) => {
-  let result: any;
-  if(req.body['id'] != null){
-    result = await prisma.sim_history.findUnique({
-      where: {
-        id: req.body['id'],
-      },
-      include: {
-        events: true,
-      }
-    });
-  } else {
-    result = await prisma.sim_history.findMany()
-  }
+// app.get('/simulation/history', async (req, res) => {
+//   let result: any;
+//   if(req.body['id'] != null){
+//     result = await prisma.sim_history.findUnique({
+//       where: {
+//         id: req.body['id'],
+//       },
+//       include: {
+//         events: true,
+//       }
+//     });
+//   } else {
+//     result = await prisma.sim_history.findMany()
+//   }
   
-  res.send(result);
-})
+//   res.send(result);
+// })
 
-app.delete('/simulation/history', async (req, res) => {
-  const result = await prisma.sim_history.deleteMany()
-  res.send(result);
-})
+// app.delete('/simulation/history', async (req, res) => {
+//   const result = await prisma.sim_history.deleteMany()
+//   res.send(result);
+// })
 
 // app.get('/simulation/event', async (req, res) => {
 //   const results = await prisma.sim_event.findMany({
@@ -48,17 +49,31 @@ app.delete('/simulation/history', async (req, res) => {
 //   res.send(results);
 // })
 
-app.post('/simulation/event', async (req, res) => {
-  const results = await prisma.sim_event.create({
+// app.post('/simulation/event', async (req, res) => {
+//   const results = await prisma.sim_event.create({
+//     data: {
+//       user_id: req.body['user_id'],
+//       action: req.body['action'],
+//       history_id: req.body['history_id']
+//     },
+//   });
+//   res.send(results);
+// })
+
+app.post('/simulation/user', async (req, res) => {
+  const body = req.body
+  const result = await prisma.user.create({
     data: {
-      user_id: req.body['user_id'],
-      action: req.body['action'],
-      history_id: req.body['history_id']
+      id: uuidv4(),
+      firstname: body['firstname'],
+      lastname: body['lastname'],
+      addition: body['addition'],
+      role: m.Roles[body['role'] as keyof typeof m.Roles],
     },
   });
-  res.send(results);
+  res.send(result);
 })
 
-const server = app.listen(3000, () =>
+const server = app.listen(port, () =>
   console.log(`🚀 Server ready at: http://localhost:3000`),
 );
