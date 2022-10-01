f8d85f315cd871a8a9f07311cb38c69819257f02
M. Lammers <60792074+martijnlammers@users.noreply.github.com>
2022-10-01 19:31:13 +0200

    Finished user CRUD.

--- src/server.ts
+++ src/server.ts
@@ -3,62 +3,50 @@ import { PrismaClient } from "@prisma/client";
 import { v4 as uuidv4 } from "uuid";
 import * as m from "./models";
 
-const app = express();
-app.use(express.json());
+const app = express().use(express.json());
 const port = 3000;
 const prisma = new PrismaClient();
 
-// app.post('/simulation/history', async (req, res) => {
-//   const result = await prisma.sim_history.create({
-//     data: {
-//       id: `${uuidv4()}`,
-//     }
-//   });
-//   res.send(result);
-// })
+app.post("/simulation/session", async (req, res) => {
+  const data = req.body;
+  let result: any;
+  try {
+    result = await prisma.session.create({ data });
+    res.send(result);
+  } catch (e) {
+    console.log(e);
+    res.send(e);
+  }
+  return;
+});
 
-// app.get('/simulation/history', async (req, res) => {
-//   let result: any;
-//   if(req.body['id'] != null){
-//     result = await prisma.sim_history.findUnique({
-//       where: {
-//         id: req.body['id'],
-//       },
-//       include: {
-//         events: true,
-//       }
-//     });
-//   } else {
-//     result = await prisma.sim_history.findMany()
-//   }
+app.put("/simulation/session", async (req, res) => {
+  const data = req.body;
+  let result: any;
+  try {
+    result = await prisma.session.update({
+      where: {
+        id: req.body["id"],
+      },
+      data,
+    });
+    res.send(result);
+  } catch (e) {
+    console.log(e);
+    res.send(e);
+  }
+});
 
-//   res.send(result);
-// })
-
-// app.delete('/simulation/history', async (req, res) => {
-//   const result = await prisma.sim_history.deleteMany()
-//   res.send(result);
-// })
-
-// app.get('/simulation/event', async (req, res) => {
-//   const results = await prisma.sim_event.findMany({
-//     where: {
-//       user_id: req.body['user_id'],
-//     },
-//   });
-//   res.send(results);
-// })
-
-// app.post('/simulation/event', async (req, res) => {
-//   const results = await prisma.sim_event.create({
-//     data: {
-//       user_id: req.body['user_id'],
-//       action: req.body['action'],
-//       history_id: req.body['history_id']
-//     },
-//   });
-//   res.send(results);
-// })
+app.get("/simulation/session", async (req, res) => {
+  const results = await prisma.session.findMany({
+    include:{
+      participants: true,
+      events: true,
+      scene: true
+    }
+  });
+  res.send(results);
+});
 
 app.post("/simulation/user", async (req, res) => {
   const data = req.body;
