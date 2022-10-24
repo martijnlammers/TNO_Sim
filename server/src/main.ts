8dbfcbe72be0cc9d64381bffc363e1596e599e08
M. Lammers <60792074+martijnlammers@users.noreply.github.com>
2022-10-24 17:58:42 +0200

    Add swagger docs on /api

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('TNO Database calls')
+    .setDescription('API calls used for creating and reading historic simulation data.')
+    .setVersion('1.0')
+    .addTag('TNO')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
