b2c8520e3c57f0e00cb8121508a3be815183a4b4
Martijn Lammers <m.b.alyanak@gmail.com>
2023-01-05 19:44:06 +0000

    fix: remove non-existing import.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,13 +5,12 @@ import { SessionController } from './session/session.controller';
 import { SceneService } from './scene/scene.service';
 import { SessionService } from './session/session.service';
 import { UserService } from './user/user.service';
-import { PrefabController } from './prefab/prefab.controller'
 import { UnityService } from './unity/unity.service';
-import { UnityController } from './unity/prefab.controller';
+import { UnityController } from './unity/unity.controller';
 
 @Module({
   imports: [],
-  controllers: [PrefabController, UserController, SceneController, SessionController, UnityController],
+  controllers: [UserController, SceneController, SessionController, UnityController],
   providers: [SceneService, SessionService, UserService, UnityService],
 })
 export class AppModule {}
