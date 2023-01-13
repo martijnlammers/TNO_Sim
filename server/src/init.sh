234e9b947684d24a69a1215b5d07fd929c7fa98a
Martijn Lammers <m.b.alyanak@gmail.com>
2023-01-13 12:59:22 +0000

    fix: renewed commands init.sh

--- server/src/init.sh
+++ server/src/init.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-cd ..
+# set PORT=80
+# set HOST=0.0.0.0
+# set DATABASE_URL=postgresql://postgres:postgres@postgres:5432/TNO_SIM_DB
+cd server
 npm i
-cd ./prisma
+cd prisma
 npx prisma generate
 npx prisma migrate dev --name init
-cd .. 
+cd ..
 npm run start:dev
